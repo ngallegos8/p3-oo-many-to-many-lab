240697ca9aeb852af6ea768de2ca2dc16ff3ae1a
ngallegos8 <152644226+ngallegos8@users.noreply.github.com>
2024-02-05 11:03:05 -0700

    lab complete. 14 tests pass

--- lib/many_to_many.py
+++ lib/many_to_many.py
@@ -1,10 +1,97 @@
 class Author:
-    pass
+
+    all = []
+
+    def __init__(self, name):
+        self.name = name
+        Author.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.author == self]
+
+    def books(self):
+        return [contract.book for contract in self.contracts()]
+        # return [contract.book for contract in self.contracts()]
+
+    def sign_contract(self, book, date, royalties):
+        return Contract(self, book, date, royalties)
+    
+    def total_royalties(self):
+        return sum([contract.royalties for contract in self.contracts()])
 
 
 class Book:
-    pass
+
+    all = []
+
+    def __init__(self, title):
+        self.title = title
+        Book.all.append(self)
+
+    def contracts(self):
+        return [contract for contract in Contract.all if contract.book == self]
+    
+    def authors(self):
+        return [contract.author for contract in self.contracts()]
 
 
 class Contract:
-    pass
+
+    all = []
+
+    def __init__(self, author, book, date, royalties):
+        self.author = author
+        self.book = book
+        self.date = date
+        self.royalties = royalties 
+        Contract.all.append(self)
+
+    @property
+    def author(self):
+        return self._author
+    @author.setter
+    def author(self, value):
+        if isinstance(value, Author):
+        # ALT VERSION
+        # if type(value) is Author:
+            self._author = value
+        else:
+            raise Exception
+        
+    @property
+    def book(self):
+        return self._book
+    @book.setter
+    def book(self, value):
+        if isinstance(value, Book):
+        # ALT VERSION
+        # if type(value) is Book:
+            self._book = value
+        else:
+            raise Exception
+        
+    @property
+    def date(self):
+        return self._date
+    @date.setter
+    def date(self, value):
+        if isinstance(value, str):
+        # ALT VERSION
+        # if type(value) is str:
+            self._date = value
+        else:
+            raise Exception
+        
+    @property
+    def royalties(self):
+        return self._royalties
+    @royalties.setter
+    def royalties(self, value):
+        if isinstance(value, int):
+            self._royalties = value
+        else:
+            raise Exception
+    
+    @classmethod
+    def contracts_by_date(cls, date):
+        return [contract for contract in cls.all if contract.date == date]
